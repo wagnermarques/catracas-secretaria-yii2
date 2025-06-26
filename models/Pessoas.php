8a1398b2ece827363bbefa0c9ca1625768fea7f2
wagnermarques <wagnerdocri@gmail.com>
2025-06-25 22:07:23 -0300

    ajustes na validacao no model Pessoas

--- models/Pessoas.php
+++ models/Pessoas.php
@@ -38,6 +38,19 @@ class Pessoas extends \yii\db\ActiveRecord
             [['emailpessoal'], 'email'],
             [['firstname', 'lastname'], 'string', 'max' => 100],
             [['rg', 'cpf'], 'string', 'max' => 20],
+            
+            [['emailpessoal'], 'unique'],  
+            [['rg'], 'unique'],  
+            [['cpf'], 'unique'],  
+            [['firstname', 'lastname', 'emailpessoal', 'idade', 'rg', 'cpf'], 'trim'],
+            [['firstname', 'lastname', 'emailpessoal', 'rg', 'cpf'], 'default', 'value' => null],
+            [['idade'], 'default', 'value' => 0],
+            [['firstname'], 'match', 'pattern' => '/^[a-zA-Z\s]+$/u', 'message' => 'O primeiro nome deve conter apenas letras e espaços.'],
+            [['lastname'], 'match', 'pattern' => '/^[a-zA-Z\s]*$/u', 'message' => 'O sobrenome deve conter apenas letras e espaços.'],
+            [['emailpessoal'], 'match', 'pattern' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', 'message' => 'O email pessoal deve ser um endereço de email válido.'],
+            [['rg'], 'match', 'pattern' => '/^\d{1,20}$/', 'message' => 'O RG deve conter apenas números e ter no máximo 20 dígitos.'],
+            [['cpf'], 'match', 'pattern' => '/^\d{11}$/', 'message' => 'O CPF deve conter exatamente 11 dígitos.'],
+            [['cpf'], 'validateCpf'],                   
         ];
     }
 
